9f601dea346e387c7e49448860e776fc0d06cf2e
TURAGA VISHNU SAHITYA <156160713+22PA1A05G7@users.noreply.github.com>
2024-12-24 20:06:47 +0530

    PaymentService.java

--- Hotel ManagementSystem(LLD)/PaymentService.java
+++ Hotel ManagementSystem(LLD)/PaymentService.java
@@ -1,5 +1,6 @@
 public class PaymentService {
     public void processPayment(double amount) {
         System.out.println("Payment processed for amount: $" + amount);
+        return true;
     }
 }
